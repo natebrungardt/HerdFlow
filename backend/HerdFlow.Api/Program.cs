8e921a2455b811f5c4263dfda99a57cf4901c143
Nate <natebrungardt15@gmail.com>
2026-03-30 11:29:20 -0500

    setup docker local db + dev config

--- backend/HerdFlow.Api/Program.cs
+++ backend/HerdFlow.Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers()
