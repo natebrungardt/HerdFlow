b71b9c844bf8977333a0142a35c6ee23a1185fd3
Nate <natebrungardt15@gmail.com>
2026-04-14 14:49:15 -0500

    add initial iOS app scaffold (SwiftUI)

---
+++ apps/ios/herdflow/herdflow/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  herdflow
+//
+//  Created by Nate Brungardt on 4/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("HerdFlow iOS App")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apps/ios/herdflow/herdflow/herdflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  herdflowApp.swift
+//  herdflow
+//
+//  Created by Nate Brungardt on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct herdflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
